bdef17942f2ba4248cfcb9554af20f7f683d9e2f
Hernan VQ <hernanvqt@gmail.com>
2026-09-22 00:16:55 +0200

    practica01 ejercicio 2a terminado

--- practica01/src/practica01/main.py
+++ practica01/src/practica01/main.py
@@ -1,14 +1,14 @@
 # Curso 2026-27 - FIA
 # Programa que devuelve el hito más relevante de la IA dado un año
-# Grupo:
-# Nombres y apellidos: Trajano Arthur Iacob, Hernan Velarde Quiro
+# Grupo: B
+# Nombres y apellidos: Trajano Arthur Iacob, Hernan Velarde Quiros
 #
 
 
 # Ejercicio 1
 # a) (respuesta al apartado a)
-# Ninguno ha sido capaz de pasar por humano. 
-# La razon por la que Eliza no pasa el test de Turing es porque 
+# Ninguno ha sido capaz de pasar por humano.
+# La razon por la que Eliza no pasa el test de Turing es porque
 # repite mucho algunas sentencias ignorando al entrevistador
 # con la intencion de seguir su guion.
 # Parry por otro lado, tiene un guion que impide que sea mas libre
@@ -17,22 +17,24 @@
 
 # b) (respuesta al apartado b)
 # Parry es mejor que las dos porque el LLM le ayuda mucho. Entre las dos
-# Eliza es muy repititiva y siempre utiliza las mismas frase cambiando 
+# Eliza es muy repititiva y siempre utiliza las mismas frase cambiando
 # un par de palabras con las que usaste antes, Alicia lo disfraza mejor
 #
 # c) (respuesta al apartado c)
 #
 # No parecen dos humanos, porque ambos son muy insistentes con sus temas,
 # Elizia repite constamente las mismas frases y Parry pese a pecar de lo
-# mismo, es capaz de darse cuenta de que Elizia se repite por lo que es 
+# mismo, es capaz de darse cuenta de que Elizia se repite por lo que es
 # mejor.
 
 # Ejercicio 2
 
 def pide_datos():
-    anios = []
-    anios = input("Hola, introduce varios anios y te dire los hitos mas importantes de la IA (introduce 0 para salir): ")
-    return anios
+    anios = input("Hola, introduce varios anios y te dire los hitos mas importantes de la IA (introduce 0 para salir): ").strip()
+    if (anios == "0"):
+        return []
+    else:
+        return anios.split()
 
 
 def extrae_datos_linea(linea):
@@ -46,7 +48,7 @@ def extrae_datos_fichero(nombre_fichero):
         for linea in f:
             hito = extrae_datos_linea(linea)
             datos.append(hito)
-    return datos
+    return datos[1:]
 
 
 def muestra_hitos(lista_hitos):
@@ -65,7 +67,8 @@ def busca_anio_en_lista(lista_hitos, anio):
     while (start <= end):
         medio = (start + end) // 2
         startHito = int(lista_hitos[medio]["inicio"])
-        if (startHito == anio_int):
+        endHito = int(lista_hitos[medio]["fin"])
+        if (startHito == endHito and startHito == anio_int):
             return lista_hitos[medio]["texto"]
         elif (startHito < anio_int):
             start = medio + 1
@@ -73,23 +76,22 @@ def busca_anio_en_lista(lista_hitos, anio):
             end = medio-1
 
     for hito in lista_hitos:
-        if int(hito["inicio"]) < anio_int < int(hito["fin"]):
+        if int(hito["inicio"]) <= anio_int <= int(hito["fin"]):
             return hito["texto"]
 
     return "no tengo información suficiente"
 
 
 def imprime_hito(hito, anio):
-    print(f"En {anio} {hito['texto']}")
+    print(f"En {anio} {hito}")
 
 
 def main():
     print('Ejercicio 2a: Bienvenido al Programa modular de historia de la IA')
-    print("Bienvenido al Programa modular de historia de la IA")
     lista_anios = pide_datos()
     lista_hitos = extrae_datos_fichero("historiaIAg2.csv")
 
-    while (lista_anios != "0"):
+    while (len(lista_anios) > 0):
         for anio in lista_anios:
             hito = busca_anio_en_lista(lista_hitos, anio)
             imprime_hito(hito, anio)
